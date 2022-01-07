42f91774b52cd91c3383a025e56aa3da0e4cd000
hunjixin <1084400399@qq.com>
2022-01-07 17:53:27 +0800

    add rpc

--- src/proof_rpc/proof.rs
+++ src/proof_rpc/proof.rs
@@ -1,20 +1,24 @@
 use jsonrpc_core::Result;
 use jsonrpc_derive::rpc;
-
-build_rpc_trait! {
-    pub trait ProofRpc {
-        /// Adds two numbers and returns a result
-        #[rpc(name = "add")]
-        fn add(&self, a: u64, b: u64) -> Result<u64>;
-    }
+use jsonrpc_http_server::jsonrpc_core::{IoHandler};
+#[rpc]
+pub trait ProofRpc {
+    /// Adds two numbers and returns a result
+    #[rpc(name = "add")]
+    fn add(&self, a: u64, b: u64) -> Result<u64>;
 }
 
-pub struct ProofImpl{
 
-}
+pub struct ProofImpl{}
 
 impl ProofRpc for ProofImpl {
     fn add(&self, a: u64, b: u64) -> Result<u64> {
         Ok(a + b)
     }
+}
+
+pub fn register(mut io: IoHandler) -> IoHandler {
+    let proof_impl = ProofImpl{};
+    io.extend_with(proof_impl.to_delegate());
+    return io
 }
